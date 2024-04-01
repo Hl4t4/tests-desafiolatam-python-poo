249c25e5a31f462ccd8d0623e3fedc608f5e9814
Jose Latapiatt <jose.latapiatt@proton.me>
2024-04-01 07:40:55 -0300

    Agregada clase te

---
+++ desafio-opcional/te.py
@@ -0,0 +1,28 @@
+class Te():
+    sabor = int
+    tiempo = int
+    descripcion = str
+    formato = int
+
+    @staticmethod
+    def tiempo_y_recomendacion (sabor):
+        #Te negro
+        if sabor == 1:
+            return 3, "Se recomienda consumir al desayuno"
+        #Te verde
+        elif sabor == 2:
+            return 5, "Se recomienda consumir al medio dia"
+        #Agua de hierbas
+        elif sabor == 3:
+            return 6, "Se recomienda consumir al atardecer"
+        else:
+            return 0, "Valor no valido"
+    
+    @staticmethod
+    def tiempo_y_recomendacion (formato):
+        if formato == 300:
+            return 3000
+        elif formato == 500:
+            return 5000
+        else:
+            return 0
