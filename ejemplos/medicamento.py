8ad4e7e70b31fdce66ca44105e19417f9f532d05
Jose Latapiatt <jose.latapiatt@proton.me>
2024-04-03 13:56:53 -0300

    actualizacion a ejemplo de medicamentos poo

--- ejemplos/medicamento.py
+++ ejemplos/medicamento.py
@@ -1,11 +1,40 @@
 # archivo medicamento.py
 class Medicamento():
-    descuento = 0.05
     IVA = 0.18
+    def __init__(self, nombre : str, stock: int = 0):
+        self.descuento = 0.0
+        self.precio_bruto = 0
+        self.precio_final = 0.0
+        self.nombre = nombre
+        self.stock = stock
+
+    def __eq__(self, other):
+        return self.nombre.lower() == other.nombre.lower()
+    
+    def __iadd__(self, other):
+        if self == other:
+            self.stock += other.stock
+        return self
 
     @staticmethod
     def validar_mayor_a_cero(numero: int):
         return numero > 0
+
+    @property
+    def precio(self):
+        return self.precio_final
+    
+    @precio.setter
+    def precio(self, precio_bruto: int):
+        if self.validar_mayor_a_cero(precio_bruto):
+            self.precio_bruto = precio_bruto
+            precio_final = precio_bruto * (1+self.IVA)
+            if precio_final >= 10000 and self.precio < 20000:
+                self.descuento = 0.1
+            elif precio_final >= 20000 and self.precio < 30000:
+                self.descuento = 0.2
+            self.precio_final = precio_final * (1-self.descuento)
+
     
     def asigna_precio(self, precio_entregado: int):
         es_valido = self.validar_mayor_a_cero(precio_entregado)
--- ejemplos/programa.py
+++ ejemplos/programa.py
@@ -1,17 +1,54 @@
 from medicamento import Medicamento
 
-precio = int(input("Ingrese un precio a validar:\n"))
-es_valido = Medicamento.validar_mayor_a_cero(precio)
+# precio = int(input("Ingrese un precio a validar:\n"))
+# es_valido = Medicamento.validar_mayor_a_cero(precio)
 
-if es_valido:
-    print("El precio ingresado es válido")
-else:
-    print("El precio ingresado no es válido")
+# if es_valido:
+#     print("El precio ingresado es válido")
+# else:
+#     print("El precio ingresado no es válido")
 
-m1 = Medicamento()
-m2 = Medicamento()
+# m1 = Medicamento()
+# m2 = Medicamento()
 
-if m1.IVA == m2.IVA and m1.descuento == m2.descuento:
-    print("Todas las instancias tienen igual descuento e IVA")
-    print("El valor del IVA es: ", Medicamento.IVA)
-    print("El valor del descuento es:", Medicamento.descuento)
+# if m1.IVA == m2.IVA and m1.descuento == m2.descuento:
+#     print("Todas las instancias tienen igual descuento e IVA")
+#     print("El valor del IVA es: ", Medicamento.IVA)
+#     print("El valor del descuento es:", Medicamento.descuento)
+
+# nombre = input("Ingrese nombre del medicamento:\n")
+# stock = int(input("Ingrese stock del medicamento:\n"))
+# precio_bruto = int(input("Ingrese precio bruto del medicamento:\n"))
+
+# m1 = Medicamento(nombre, stock)
+# m1.precio = precio_bruto
+
+# print(f"El precio bruto del medicamento {m1.nombre} es {m1.precio_bruto}")
+# if m1.descuento:
+#     print(f"Tiene un descuento de {m1.descuento * 100}%")
+
+# print(f"El precio final del medicamento es {m1.precio_final}")
+
+opcion_ingreso = int(input("¿Desea agregar un medicamento?"
+"\n1. Sí\n2. No\n"))
+ingresados = []
+
+while opcion_ingreso == 1:
+    nombre = input("\nIngrese nombre del medicamento:\n")
+    stock = int(input("\nIngrese stock del medicamento:\n"))
+    m = Medicamento(nombre, stock)
+    if m in ingresados:
+        indice = ingresados.index(m)
+        ingresados[indice] += m
+    else:
+        ingresados.append(m)
+        precio_bruto = int(input("\nIngrese precio bruto del medicamento:\n"))
+        m.precio = precio_bruto
+    print(f"\n***** DATOS MEDICAMENT1O {m.nombre} *****")
+    print(f"PRECIO BRUTO: ${m.precio_bruto}")
+    if m.descuento:
+        print(f"DESCUENTO: {m.descuento*100}%")
+    print(f"PRECIO FINAL: ${m.precio_final}")
+    print(f"STOCK: {m.stock}")
+    print(f"\nLa farmacia cuenta con {len(ingresados)} medicamento(s)\n")
+    opcion_ingreso = int(input("¿Desea agregar un medicamento? \n1. Sí\n2. No\n"))
